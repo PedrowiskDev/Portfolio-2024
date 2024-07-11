a8698a8b891bc5ccd8d5e9e5f37dd49b18ed5e38
PedrowiskDev <ph6304112@gmail.com>
2024-07-11 17:10:16 -0300

    teste

--- portfolio-2024/src/app/page.tsx
+++ portfolio-2024/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[url('/pattern.png')] bg-cover bg-fixed">
       <div>
         <p className="text-5xl font-bold font-mono text-white">
-          Pedro Henrique Caetano Soares
+          Pedro Henrique Caetano Soares.
         </p>
       </div>
     </main>
